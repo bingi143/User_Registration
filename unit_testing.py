875cf507c551a692d31f895b32280f1f7bc509c5
bingi143 <bingivenkateswarlu578@gmail.com>
2024-09-04 13:48:36 +0530

    [venkatesh] Add:unit test for first name

---
+++ unit_testing.py
@@ -0,0 +1,30 @@
+'''
+
+@Author: Venkatesh
+@Date: 2024-09-04 18:00:30
+@Last Modified by: Venkatesh
+@Last Modified time: 2024-09-04 18:00:30
+@Title : Program Aim to unit testing on user registration
+
+
+''' 
+
+import unittest
+from user_registration import checking_first_name
+ 
+class TestingUserRegistarion(unittest.TestCase):
+    
+    def test_first_name(self):
+        self.assertTrue(checking_first_name("arn"))
+        self.assertFalse(checking_first_name("23Vwe"))
+        self.assertTrue(checking_first_name("VenKy"))
+        self.assertFalse(checking_first_name("Juewf"))
+
+
+def main():
+    t1=TestingUserRegistarion()
+    t1.test_first_name()
+
+
+if __name__=="__main__":
+     unittest.main()
