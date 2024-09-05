4731d8f1b791e7d2bca4d7d5df4f5516b74100af
bingi143 <bingivenkateswarlu578@gmail.com>
2024-09-05 16:59:01 +0530

    [Venkatesh] Add: unit testing password upper charecters

--- unit_testing.py
+++ unit_testing.py
@@ -15,18 +15,42 @@ from user_registration import checking_first_name,checking_last_name,email_verif
 class TestingUserRegistarion(unittest.TestCase):
     
     def test_first_name(self):
+        '''
+          Description: 
+                This function is testing the first name have start with capital
+          Parameters: 
+                None
+          Return : 
+                None
+        '''
         self.assertTrue(checking_first_name("Monkey"))
         self.assertFalse(checking_first_name("23Vwe"))
         self.assertTrue(checking_first_name("Venkatesh"))
         self.assertFalse(checking_first_name("juewf"))
 
     def test_last_name(self):
+        '''
+          Description: 
+                This function is testing the last name have start with capital
+          Parameters: 
+                None
+          Return : 
+                None
+        '''
         self.assertTrue(checking_last_name("Bingi"))
         self.assertFalse(checking_last_name("ponkey"))
         self.assertTrue(checking_last_name("Sweet"))
         self.assertFalse(checking_last_name("venkat"))
 
     def test_email(self):
+        '''
+          Description: 
+                This function is testing the email
+          Parameters: 
+                None
+          Return : 
+                None
+        '''
         self.assertTrue(email_verification("venky@bl.co.in"))
         self.assertTrue(email_verification("abc.xyz@bl.co"))
         self.assertTrue(email_verification("123@bl.co"))
@@ -35,6 +59,14 @@ class TestingUserRegistarion(unittest.TestCase):
         self.assertFalse(email_verification("Venky@blco."))
     
     def test_mobile_number(self):
+        '''
+          Description: 
+                This function is testing the mobile number having 10 and space for capital code
+          Parameters: 
+                None
+          Return : 
+                None
+        '''
         self.assertTrue(check_mobile_formet("91 6305114038"))
         self.assertTrue(check_mobile_formet("88 5453534212"))
         self.assertTrue(check_mobile_formet("71 9087654312"))
@@ -43,6 +75,14 @@ class TestingUserRegistarion(unittest.TestCase):
         self.assertFalse(check_mobile_formet("8 6305114038"))
     
     def test_mail(self):
+        '''
+          Description: 
+                This function is testing the mail at least one upper case
+          Parameters: 
+                None
+          Return : 
+                None
+        '''
         self.assertTrue(checking_password("We345sdf24"))
         self.assertTrue(checking_password("12234F35342"))
         self.assertTrue(checking_password("QWERTYUIOP"))
